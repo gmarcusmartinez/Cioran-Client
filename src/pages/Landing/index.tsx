2723a80d64afb7ee35d08e5f6ad75cba3941517a
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-10-21 20:57:19 -0500

    created Media Query Manager for a mobile first design and apply styles to Landing Page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,18 +1,21 @@
 import AuthCard from "components/Landing/AuthCard";
 import React from "react";
 
+const headerText = "Cioran";
+const slogan =
+  "Plan, track, and manage your agile and software development projects in Cioran. Customize your workflow, collaborate, and release greatsoftware.";
+
 const Landing = () => {
   return (
     <div className="landing-screen">
       <div className="hero">
-        <div className="hero__header"></div>
+        <div className="hero__header">
+          <h1 className="header-logo">{headerText}</h1>
+          <h1 className="header-slogan">{slogan}</h1>
+        </div>
         <div className="hero__display">
-          <div className="hero__logo">Cioran</div>
-          <p className="hero__slogan">
-            Plan, track, and manage your agile and software development projects
-            in Cioran. Customize your workflow, collaborate, and release great
-            software.
-          </p>
+          <div className="hero__logo">{headerText}</div>
+          <p className="hero__slogan">{slogan}</p>
         </div>
         <div className="hero__auth">
           <AuthCard />
