46f1af06073b8cf07e3a81445661c5d50a4f6414
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-12-28 12:23:20 -0600

    UserCreated event added

--- common/src/events/project-created-event.ts
+++ common/src/events/project-created-event.ts
@@ -1,11 +1,19 @@
 import { Subjects } from "../subjects";
 
+interface teamMember {
+  id: String;
+  name: String;
+  avatar: String;
+  role: String;
+}
+
 export interface ProjectCreatedEvent {
   subject: Subjects.ProjectCreated;
   data: {
     id: string;
     title: string;
     slug: string;
+    team: teamMember[];
     projectOwner: string;
   };
 }
