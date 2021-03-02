b1072a510587b209b292b6f72026e313024c2640
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-03-01 19:39:50 -0500

    UserCreated event added

--- common/build/subjects/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Subjects = void 0;
-var Subjects;
-(function (Subjects) {
-    Subjects["ProjectCreated"] = "project:created";
-    Subjects["ProjectUpdated"] = "project:updated";
-    Subjects["UserCreated"] = "user:created";
-})(Subjects = exports.Subjects || (exports.Subjects = {}));
