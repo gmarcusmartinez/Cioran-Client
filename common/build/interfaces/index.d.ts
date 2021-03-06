e0e8d3fcf6d77cf4385a9d82f761f272b84ddb5f
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-03-06 17:07:53 -0500

    UserCreated event added

--- common/build/interfaces/index.d.ts
+++ common/build/interfaces/index.d.ts
@@ -2,3 +2,9 @@ export interface IUserProject {
     _id: string;
     title: string;
 }
+export interface IProjectSprint {
+    _id: string;
+    title: string;
+    startDate: Date;
+    endDate: Date;
+}
