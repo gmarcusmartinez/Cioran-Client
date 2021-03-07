19e38dae57fee08e997fa6d3097060e84aef5791
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-03-06 20:23:42 -0500

    ITeamMember interface added

--- common/build/events/project-created-event.d.ts
+++ common/build/events/project-created-event.d.ts
@@ -1,21 +1,12 @@
+import { ITeamMember } from '../interfaces';
 import { Subjects } from '../subjects';
-export declare enum RoleType {
-    Admin = "admin",
-    Member = "member"
-}
-export interface TeamMember {
-    _id: string;
-    name: string;
-    avatar: string;
-    role: RoleType;
-}
 export interface ProjectCreatedEvent {
     subject: Subjects.ProjectCreated;
     data: {
         id: string;
         title: string;
         slug: string;
-        team: TeamMember[];
+        team: ITeamMember[];
         projectOwner: string;
     };
 }
--- common/build/interfaces/index.d.ts
+++ common/build/interfaces/index.d.ts
@@ -1,4 +1,12 @@
+export declare enum RoleType {
+    Admin = "admin",
+    Member = "member"
+}
 export interface IUserProject {
     _id: string;
     title: string;
+}
+export interface ITeamMember {
+    _id: string;
+    role: RoleType;
 }
